693f1ef9b2c7109dedf1b7c442d638745516b94c
hiimReb20 <86073058+hiimReb20@users.noreply.github.com>
2021-06-25 12:06:51 +0200

    Update mylib.h

--- src/mylib.h
+++ src/mylib.h
@@ -28,6 +28,8 @@
 #include "./icl_hash.h"
 
 
+static int rimpiazzi_totali=0;
+
 #define PMAX 108
 
 #define ec_meno1(r,s)       if((r)==-1){perror(s); exit(EXIT_FAILURE);}
@@ -93,19 +95,69 @@ typedef struct arg_s{
     int fd_r;
 }arg;
 
+//inizializza i campi della struct con con i valori letti nel file puntato da conffile
+//ritorna c inizializzata se ha successo, NULL altrimenti
 conf *myconf(char* conffile);
+
+//restituisce il numero corrispondente al fd maggiore presente in set
 int max_fd(fd_set set, int fd_max);
+
+//inserisce in coda a q il valore fd
 void push(coda **q, int fd);
+
+//inserisce in testa a q il valore fd 
 void push_in_testa(coda **q, int fd);
+
+//preleva il valore in testa a q e lo restituisce
 int pop(coda **q);
+
+//elimina tutti gli elementi presenti in q
 void clean_coda(coda **q);
+
 ssize_t readn(int fd, void *ptr, size_t n);
 ssize_t writen(int fd, void *ptr, size_t n);
 
+//apre il file passato attraverso fd e lo inserisce in ht
+//stampa su file di log un riassunto dell'operazione
+//comunica al Master attraverso fd_w il valore fd
 void openF(int fd, int fd_w, int i, myhash *ht);
+
+//legge il file passato attraverso fd contenuto in ht e restituisce il contenuto al client
+//stampa su file di log un riassunto dell'operazione
+//comunica al Master attraverso fd_w il valore fd
 void readFi(int fd, int fd_w, int i, myhash *ht);
+
+//legge n file contenuti in ht e ne restituisce il contenuto al client
+//n è passato dal client attraverso fd
+//se n<=0 legge e restituisce tutti i file presenti in ht
+//stampa su file di log un riassunto dell'operazione
+//comunica al Master attraverso fd_w il valore fd
 void readNF(int fd, int fd_w, int i, myhash *ht);
+
+//aggiorna il file passato attraverso fd e contenuto in ht con il contenuto
+//anch'esso passato dal client attraverso fd
+//include il file nel conteggio dei file e della dimensione se il contenuto!=NULL
+//stampa su file di log un riassunto dell'operazione
+//comunica al Master attraverso fd_w il valore fd
 void writeF(int fd, int fd_w, int i, myhash *ht);
+
+//aggiorna il file passato attraverso fd e contenuto in ht con la stringa
+//anch'essa passata dal client attraverso fd
+//include il file nel conteggio dei file e della dimensione se il contenuto!=NULL
+//se il file non rientrava già in questi calcoli
+//stampa su file di log un riassunto dell'operazione
+//comunica al Master attraverso fd_w il valore fd
 void append(int fd, int fd_w, int i, myhash *ht);
+
+//chiama la funzione hash_dump sul file di log e su ht
+//stampa su file di log alcune statistiche dell'esecuzione del programma
+//effettua la fclose sul file di log
+void chiusura(myhash *ht);
+
+//stampa sul file di log un riassunto dell'operazione di apertura connessione
+void stampa_conn(int n);
+
+//stampa sul file di log un riassunto dell'operazione di chiusura connessione
+void stampa_close(int n);
 //
 #endif
